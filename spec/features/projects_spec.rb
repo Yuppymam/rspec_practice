155e4d969f48eb88464e43cc1ef08e740bef5931
Yuppy <yuppymam@gmail.com>
2020-01-21 00:30:36 +0900

    finish chapter8

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -3,26 +3,47 @@ require 'rails_helper'
 RSpec.feature 'Projects', type: :feature do
   scenario 'ユーザーは新しいプロジェクトを作成する' do
     user = FactoryBot.create(:user)
+    sign_in user
     visit root_path
-    click_link 'Sign in'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
 
-    expect {
-      click_link 'New Project'
-      fill_in 'Name', with: 'Test Project'
-      fill_in 'Description', with: 'Trying out Capybara'
-      click_button 'Create Project'
+    expect { create_project }.to change(user.projects, :count).by(1)
 
+    aggregate_failures do
       expect(page).to have_content 'Project was successfully created'
       expect(page).to have_content 'Test Project'
       expect(page).to have_content "Owner: #{user.name}"
-    }.to change(user.projects, :count).by(1)
+    end
+  end
+
+  scenario 'ユーザーはプロジェクトを編集する' do
+    user = FactoryBot.create(:user)
+    sign_in user
+    visit root_path
+
+    create_project
+
+    expect {
+      click_link 'Edit'
+      fill_in 'Name', with: 'New Test Project'
+      click_button 'Update Project'
+    }.to_not change(user.projects, :count)
+
+    aggregate_failures do
+      expect(page).to have_content 'Project was successfully update'
+      expect(page).to have_content 'New Test Project'
+      expect(page).to have_content "Owner: #{user.name}"
+    end
   end
 
   scenario 'ゲストはプロジェクトを追加できない' do
     visit projects_path
     expect(page).to have_content 'Log in'
+  end
+
+  def create_project
+    click_link 'New Project'
+    fill_in 'Name', with: 'Test Project'
+    fill_in 'Description', with: 'Trying out Capybara'
+    click_button 'Create Project'
   end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,24 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  it 'ファクトリで関連するデータを生成する' do
-    note = FactoryBot.create(:note)
-    puts "この note の project は#{note.project.inspect}です"
-    puts "この note の user は#{note.user.inspect}です"
-  end
-  before do
-    @user = FactoryBot.create(:user)
-
-    @project = @user.projects.create(
-      name: 'Test project'
-    )
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   it 'ユーザー、プロジェクト、メッセージがあれば有効な状態である' do
     note = Note.new(
       message: 'This is a sample note.',
-         user: @user,
-      project: @project
+         user: user,
+      project: project
     )
     expect(note).to be_valid
   end
@@ -30,31 +20,38 @@ RSpec.describe Note, type: :model do
   end
 
   describe '文字列に一致するメッセージを検索する' do
-    before do
-      @note1 = @project.notes.create(
-        message: 'This is the first note.',
-           user: @user
-      )
-      @note2 = @project.notes.create(
-        message: 'This is the second note.',
-           user: @user
-      )
-      @note3 = @project.notes.create(
-        message: 'First, preheat the oven',
-           user: @user
-      )
-    end
+    let!(:note1) {
+      FactoryBot.create(:note,
+                        project: project,
+                           user: user,
+                        message: 'This is the first note.')
+    }
+
+    let!(:note2) {
+      FactoryBot.create(:note,
+                        project: project,
+                           user: user,
+                        message: 'This is the second note.')
+    }
+
+    let!(:note3) {
+      FactoryBot.create(:note,
+                        project: project,
+                           user: user,
+                        message: 'First, preheat the oven.')
+    }
 
     context '一致するデータが見つかるとき' do
       it '検索文字列に一致するメモを返す' do
-        expect(Note.search('first')).to include(@note1, @note3)
-        expect(Note.search('first')).to_not include(@note2)
+        expect(Note.search('first')).to include(note1, note3)
+        expect(Note.search('first')).to_not include(note2)
       end
     end
 
     context '一致するデータが1件も見つからないとき' do
       it '空のコネクションを返す' do
         expect(Note.search('message')).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
