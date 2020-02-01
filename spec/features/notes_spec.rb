377fbe1d8bb5d0dd08ce5bd9d3233a85c7e829d2
Yuppy <yuppymam@gmail.com>
2020-02-01 19:51:55 +0900

    add spec/files

--- spec/features/notes_spec.rb
+++ spec/features/notes_spec.rb
@@ -1,26 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature 'Notes', type: :feature do
-  scenario 'ユーザーは新しいノートを作成できる', js: true do
-    user = FactoryBot.create(:user)
-    FactoryBot.create(:project, name: 'RSpec tutorial',
-                               owner: user)
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) {
+    FactoryBot.create(:project,
+                      name: 'RSpec tutorial',
+                     owner: user)
+  }
 
-    visit root_path
-    click_link 'Sign in'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-    click_link 'RSpec tutorial'
-
-    expect {
-      click_link 'Add Note'
-      fill_in 'Message', with: 'Test Note'
-      click_button 'Create Note'
-
-      expect(page).to have_content 'Note was successfully created'
-      expect(page).to have_content 'Test Note'
-      expect(page).to have_content "Owner: #{user.name}"
-    }.to change(user.notes, :count).by(1)
+  scenario 'ユーザーが添付ファイルをアップロードする' do
+    sign_in user
+    visit project_path(project)
+    click_link 'Add Note'
+    fill_in 'Message', with: 'My book cover'
+    attach_file 'Attachment', "#{Rails.root}/spec/files/attachment.jpg"
+    click_button 'Create Note'
+    expect(page).to have_content 'Note was successfully created'
+    expect(page).to have_content 'My book cover'
+    expect(page).to have_content 'attachment.jpg (image/jpeg'
   end
 end
